c73f767fec03149aa5fef3c5ed7726fd2ae5f161
bsc-com-32-20 <bsc-com-32-20@unima.ac.mw>
2025-02-03 10:10:13 +0200

    Implemented the book class

--- Book.java
+++ Book.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class Book {
-    
+    private static int number_of_books = 1;
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+    private static ArrayList<Book> books = new ArrayList<Book>();
+
+    public Book(String title, String author, String publisher, int year) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        number_of_books++;
+    }
+    public static int getNumberOfBooks() {
+        return number_of_books;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public static void addBook(Book book) {
+        books.add(book);
+    }
+
+    public static void removeBook(String book_title) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getTitle().toLowerCase().equals(book_title.toLowerCase())) {
+                books.remove(i);
+                System.out.println("Book removed");
+                break;
+            }
+        }
+    }
 }
--- Library.java
+++ Library.java
@@ -4,11 +4,11 @@ public class Library {
     public static void main(String[] args) {
         boolean isInSystem = true;
         System.out.println("""
-        Welcome to the Codecamp Library System 
+                Welcome to the Codecamp Library System
 
-        """);
-        while(isInSystem){
-            if(isInSystem){
+                """);
+        while (isInSystem) {
+            if (isInSystem) {
                 System.out.println("""
                         1. Add a book
                         2. Remove a book
@@ -19,22 +19,35 @@ public class Library {
                         7. Exit
                         """);
                 Scanner sc = new Scanner(System.in);
-                int choice = sc.nextInt();
-                if(choice == 1){}
-                else if(choice == 2){}
-                else if(choice == 3){}
-                else if(choice == 4){}
-                else if(choice == 5){}
-                else if(choice == 6){}
-                else if(choice == 7){
-                    isInSystem = false;
-                    System.out.println("Goodbye!"); 
+                try {
+                    int choice = sc.nextInt();
+                    if (choice == 1) {
+
+                    } else if (choice == 2) {
+
+                    } else if (choice == 3) {
+
+                    } else if (choice == 4) {
+
+                    } else if (choice == 5) {
+
+                    } else if (choice == 6) {
+
+                    } else if (choice == 7) {
+                        isInSystem = false;
+                        System.out.println("Goodbye!");
+                        continue;
+                    } else {
+                        System.out.println("Invalid choice. Please choose a valid option.");
+                        continue;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a number.\n");
                     continue;
                 }
-            }
-            else{
-                break;
+
             }
         }
+
     }
 }
