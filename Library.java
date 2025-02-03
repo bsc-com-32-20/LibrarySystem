52bad242e68cf5e02f014192d341d23cb815de12
bsc-com-32-20 <bsc-com-32-20@unima.ac.mw>
2025-02-03 09:37:05 +0200

    Created a Library template Made the template Created the files for Book and Borrower classes

---
+++ Library.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Library {
+    public static void main(String[] args) {
+        boolean isInSystem = true;
+        System.out.println("""
+        Welcome to the Codecamp Library System 
+
+        """);
+        while(isInSystem){
+            if(isInSystem){
+                System.out.println("""
+                        1. Add a book
+                        2. Remove a book
+                        3. Display all books
+                        4. Add a borrower
+                        5. Remove a borrower
+                        6. Display all borrowers
+                        7. Exit
+                        """);
+                Scanner sc = new Scanner(System.in);
+                int choice = sc.nextInt();
+                if(choice == 1){}
+                else if(choice == 2){}
+                else if(choice == 3){}
+                else if(choice == 4){}
+                else if(choice == 5){}
+                else if(choice == 6){}
+                else if(choice == 7){
+                    isInSystem = false;
+                    System.out.println("Goodbye!"); 
+                    continue;
+                }
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
